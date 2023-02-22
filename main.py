8a2dc9d5af58d1b4484c66ad18d3f00745974b37
Computer <natasha_kulchitskaya@mail.ru>
2023-02-22 20:16:17 +0300

    3

--- main.py
+++ main.py
@@ -1,9 +1,45 @@
-n = int(input())
-a = input()
-counter = 0
-for i in a.split():
-    if int(i) > 0:
-        counter += 1
-print(counter)
+class Person:
+    def __init__(self, name, gender, age,):
+        self.__name = name
+        self.__age = age
+        self.__gender = gender
 
 
+    def get_name(self):
+        return self.__name
+
+    def get_age(self):
+        return self.__age
+    def get_gender(self):
+        return self.__gender
+
+
+    def set_name(self, name):
+        self.__name = name
+
+    def set_age(self, age):
+        self.__age = age
+    def set_gender(self, gender):
+        self.__gender = gender
+
+
+    def info(self):
+        print(f'name = {self.__name}\n'
+              f'age = {self.__age}\n'
+              f'gender = {self.__gender}')
+
+
+
+person = Person('Pasha', 'man', 29)
+person1 = Person('Sasha', 'man', 27)
+person2 = Person('Natasha', 'women', 77)
+person3 = Person('Nikita', 'man', 66)
+
+
+list = [person, person1, person2, person3]
+oldest_person = person
+
+for i in list:
+    if i.get_age() > oldest_person.get_age():
+        oldest_person = i
+oldest_person.info()
